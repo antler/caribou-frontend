f2ac05d5cc1507aca2df03cb5544a0ca4d6746ad
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-01-10 15:54:22 +0100

    Fix tests

--- test/caribou/app/test/pages.clj
+++ test/caribou/app/test/pages.clj
@@ -1,43 +1,45 @@
 (ns caribou.app.test.pages
   (:use [clojure.test])
-  (:require [caribou.app.pages :as pages]
+  (:require [caribou.config :as config]
+            [caribou.app.config :as app-config]
+            [caribou.app.pages :as pages]
             [caribou.app.routing :as routing]))
 
 (deftest route-for-test
-  (let [old-routes @routing/routes]
-    (routing/clear-routes!)
-    (routing/add-route :the_googles :get "/:site/:locale/content/:color/search" identity)
-        ;; new-routes {:the_googles "/:site/:locale/content/:color/search"}]
-    
-    (testing "null input"
-      (is (= (-> @routing/routes :the_googles :path)
-             (pages/route-for :the_googles {})))
-      (is (= {}
-             (pages/select-query :the_googles {})))
-      (is (= {}
-             (pages/select-route :the_googles {}))))
-    (testing "valid input"
-      (let [base-map {:site "g"
-                      :locale "o"
-                      :color "red"}
-            query-map {:no "yes"
-                       :yes "no"}
-            full-route (pages/route-for :the_googles
-                                        (merge (assoc base-map :color "blue")
-                                               query-map))
-            base-opts (pages/select-route :the_googles
-                                          (merge base-map query-map))
-            query-opts (pages/select-query :the_googles
-                                           (merge base-map query-map))]
-        (is (= "/g/o/content/red/search"
-               (pages/route-for :the_googles base-map)))
-        (is (or (= "/g/o/content/blue/search?yes=no&no=yes"
-                   full-route)
-                (= "/g/o/content/blue/search?no=yes&yes=no"
-                   full-route)))
-        (is (= base-map base-opts))
-        (is (= query-map query-opts))))
-    (testing "invalid input"
-      (is (thrown-with-msg? Exception #"route for .* not found"
-            (pages/route-for :the_bing {}))))
-    (dosync (swap! routing/routes (constantly old-routes)))))
+  (config/with-config (app-config/default-config)
+    (let [old-routes @(config/draw :routes)]
+      (routing/clear-routes!)
+      (routing/add-route :the_googles :get "/:site/:locale/content/:color/search" identity)
+      
+      (testing "null input"
+        (is (= (-> @(config/draw :routes) :the_googles :path)
+               (pages/route-for :the_googles {})))
+        (is (= {}
+               (pages/select-query :the_googles {})))
+        (is (= {}
+               (pages/select-route :the_googles {}))))
+      (testing "valid input"
+        (let [base-map {:site "g"
+                        :locale "o"
+                        :color "red"}
+              query-map {:no "yes"
+                         :yes "no"}
+              full-route (pages/route-for :the_googles
+                                          (merge (assoc base-map :color "blue")
+                                                 query-map))
+              base-opts (pages/select-route :the_googles
+                                            (merge base-map query-map))
+              query-opts (pages/select-query :the_googles
+                                             (merge base-map query-map))]
+          (is (= "/g/o/content/red/search"
+                 (pages/route-for :the_googles base-map)))
+          (is (or (= "/g/o/content/blue/search?yes=no&no=yes"
+                     full-route)
+                  (= "/g/o/content/blue/search?no=yes&yes=no"
+                     full-route)))
+          (is (= base-map base-opts))
+          (is (= query-map query-opts))))
+      (testing "invalid input"
+        (is (thrown-with-msg? Exception #"route for .* not found"
+              (pages/route-for :the_bing {}))))
+      (dosync (swap! (config/draw :routes) (constantly old-routes))))))
--- test/caribou/app/test/routing.clj
+++ test/caribou/app/test/routing.clj
@@ -1,13 +1,16 @@
 (ns caribou.app.test.routing
   (:use [clojure.test])
-  (:require [caribou.app.routing :as routing]))
+  (:require [caribou.config :as config]
+            [caribou.app.config :as app-config]
+            [caribou.app.routing :as routing]))
 
 (deftest ^:routing routing-test
-  (routing/add-route :home :get "/" (fn [a] (str :yoyoyoyo)))
-  (routing/add-route :later :get "/later/:what" #(-> % :params :what))
-  (routing/add-route :under :get "/:under/:okay" #(str (-> % :params :under) "-" (-> % :params :okay)))
-  (routing/add-head-routes)
-  (let [router (routing/router @routing/routes)]
-    (is (= ":yoyoyoyo" (router {:uri "/" :request-method :get})))
-    (is (= "foundational" (router {:uri "/later/foundational"})))
-    (is (= "probiotic-growth" (router {:uri "/probiotic/growth" :request-method :get})))))
+  (config/with-config (app-config/default-config)
+    (routing/add-route :home :get "/" (fn [a] (str :yoyoyoyo)))
+    (routing/add-route :later :get "/later/:what" #(-> % :params :what))
+    (routing/add-route :under :get "/:under/:okay" #(str (-> % :params :under) "-" (-> % :params :okay)))
+    (routing/add-head-routes)
+    (let [router (routing/router @(config/draw :routes))]
+      (is (= ":yoyoyoyo" (router {:uri "/" :request-method :get})))
+      (is (= "foundational" (router {:uri "/later/foundational"})))
+      (is (= "probiotic-growth" (router {:uri "/probiotic/growth" :request-method :get}))))))
