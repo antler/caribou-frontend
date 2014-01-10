d82d6fab1c9ce685a5a4878a569b4eb0050ad645
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-01-10 14:27:01 +0100

    Remove obsolete test

--- test/caribou/app/test/controller.clj
+++ test/caribou/app/test/controller.clj
@@ -2,21 +2,6 @@
   (:use [clojure.test])
   (:require [caribou.app.controller :as controller]))
 
-
-(deftest session-canary-test
-  (let [old-canary @controller/session-canary
-        new-canary (fn [params] (when-let [error (get params :error)]
-                                  (str "error was " error)))]
-    (dosync (ref-set controller/session-canary new-canary))
-    (testing "Error thrown when expected."
-      (is (thrown-with-msg? Exception #"canary reported an error:"
-            (controller/render {:error true}))))
-    (testing "No error if not expected."
-      (is (seq (controller/render {:error false :template identity})))
-      (dosync (ref-set controller/session-canary (constantly false)))
-      (is (seq (controller/render {:error true :template identity}))))
-    (dosync (ref-set controller/session-canary old-canary))))
-
 (deftest render-test
   (let [defaulted (controller/render {:template identity})
         params {:status 500
